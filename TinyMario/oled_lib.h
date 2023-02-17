327d45cb14e7071c07fae47917b37bb8ff90a155
Nextraind <119508779+Nextraind@users.noreply.github.com>
2023-02-17 10:37:06 +0000

    Upgrade packages

--- TinyMario/oled_lib.h
+++ TinyMario/oled_lib.h
@@ -1,2 +1,6 @@
 
-#define CLEAR_SCL asm("cbi 0x18, 2\n")   // 0x18 
+#define CLEAR_SCL asm("cbi 0x18, 2\n")   // 0x18 = PORTB, i.e. PORTB &= ~(1<<2);
+#define CLEAR_SDA asm("cbi 0x18, 0\n")   // PORTB &= ~ (1<<0);
+#define SET_SCL asm("sbi 0x18, 2\n")    //
+#define SET_SDA asm("sbi 0x18, 0\n")
+
