06faa686b520a3ec37994bfd27ccb75d3454131d
Nextraind <119508779+Nextraind@users.noreply.github.com>
2023-02-25 13:00:32 +0000

    Added readme mirroring plugin documentation

--- TinyMario/oled_lib.h
+++ TinyMario/oled_lib.h
@@ -4,3 +4,7 @@
 #define SET_SCL asm("sbi 0x18, 2\n")    //
 #define SET_SDA asm("sbi 0x18, 0\n")
 
+#define OUTPUT_SCL asm("sbi 0x17, 2\n")   // 0x17 = DDRB, 2 = PB2 = SCL
+#define OUTPUT_SDA asm("sbi 0x17, 0\n")   // 0x17 = DDRB, 0 = PB0 = SDA
+
+#define INPUT_SCL asm("cbi 0x17, 2\n")   
